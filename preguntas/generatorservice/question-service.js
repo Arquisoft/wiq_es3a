00a6167d83f196ce18c629f0dfda284cef110a0f
Felipe Garcia <uo285267@uniovi.es>
2024-02-22 11:14:17 +0100

    Servicio generar preguntas sin respuestas falsas

--- preguntas/generatorservice/question-service.js
+++ preguntas/generatorservice/question-service.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Question = require('./question-model');
 const Template = require('./template-model');
+const axios = require('axios');
 
 // Create an instance of Express
 const app = express();
@@ -11,17 +12,61 @@ const port = 8003;
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/questiondb';
 
+const endpointUrl = 'https://query.wikidata.org/sparql';
 
 mongoose.connect(mongoUri);
+
+//Function to search a random template
+const searchRandomTemplate = async () => {
+  const template = await Template.findOne({});  
+  return template;
+}
+
+const executeQuery = async (template) => {
+  try {
+    const settings = {
+      headers: { Accept: 'application/sparql-results+json' },
+      params: { query: template.query }
+    };
+    const { data } = await axios.get(endpointUrl, settings);
+    const formattedResults = data.results.bindings.map(function(binding) {
+      return {
+          pLabel: binding.pLabel.value,
+          rLabel: binding.rLabel.value
+      };
+    });
+    return formattedResults;
+  } catch (error) {
+    console.error("Error al ejecutar la consulta SPARQL:", error);
+    return null;
+  }
+}
 
 // Define a route to generate and store a question
 app.get('/generate-question', async (req, res) => {
   try {
+    // Search a random template
+    const template = await searchRandomTemplate();
+
+    //Execute the query
+    const formattedResults = await executeQuery(template);
     
+    //Select random formatted result
+    const randomIndex = Math.floor(Math.random() * formattedResults.length);
+    const randomResult = formattedResults[randomIndex];
 
+    // Add pLabel to question string from template
+    const question = template.question.replace('^', randomResult.pLabel);
+
+    // Create a new question
+    const newQuestion = new Question({
+      question: question,
+      correctAnswer: randomResult.rLabel
+    });
+
+    newQuestion.save();
     // Return the question as a JSON response
-
-    res.json({ response: 'Perfe mi pana' });
+    res.json(newQuestion);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
