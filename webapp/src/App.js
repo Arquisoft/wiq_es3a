b770ccecd9f9370d7fc945dd9ab18a873b0677f3
UO277382 <UO277382@uniovi.es>
2024-03-02 21:59:56 +0100

    Funcionamiento correcto del Inicio
    Falta añadir fondo y botón que redirija al
    Login

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Inicio from './components/Inicio';
 //import Link from '@mui/material/Link';
 
 function App() {
@@ -15,22 +16,8 @@ function App() {
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-        Welcome to the 2024 edition of the Software Architecture course
-      </Typography>
-      {showLogin ? <Login /> : <AddUser />}
-      <Typography component="div" align="center" sx={{ marginTop: -5 }}>
-        {showLogin ? (
-          <button name="gotoregister" type="button" onClick={handleToggleView}>
-            Register
-          </button>
-        ) : (
-          <button type="button" onClick={handleToggleView}>
-            Login
-          </button>
-        )}
-      </Typography>
+      <CssBaseline />       
+      <Inicio/>   
     </Container>
   );
 }
---
+++ webapp/src/components/Inicio.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Login from './Login';
+import AddUser from './AddUser';
+import Link from '@mui/material/Link';
+
+function Inicio() {
+    const [showLogin, setShowLogin] = useState(true);
+
+  const handleToggleView = () => {
+    setShowLogin(!showLogin);
+  };
+
+  return (
+    <Container>
+        <CssBaseline />
+      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
+        Saber y Ganar
+      </Typography>
+      {showLogin ? <Login /> : <AddUser />}
+      <Typography component="div" align="center" sx={{ marginTop: 2 }}>
+        {showLogin ? (
+          <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
+            Don't have an account? Register here.
+          </Link>
+        ) : (
+          <Link component="button" variant="body2" onClick={handleToggleView}>
+            Already have an account? Login here.
+          </Link>
+        )}
+      </Typography>
+    </Container>
+    );
+}
+
+export default Inicio;
