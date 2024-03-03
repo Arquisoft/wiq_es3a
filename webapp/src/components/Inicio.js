81d13e962e0863989a47e3f38acc97ac68436c35
UO277382 <UO277382@uniovi.es>
2024-03-03 22:25:53 +0100

    Página inicio terminada.
    Los botones necesarios han sido ignorados porque ya se creo la barra de navegación para el sitio

--- webapp/src/components/Inicio.js
+++ webapp/src/components/Inicio.js
@@ -2,30 +2,21 @@ import React, { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import "./Inicio.css";
 
 function Inicio() {
-  
-  const styles =
-  {  
-    backgroundImage:"url(/background.jpg)",
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    width: "100%",
-    height: '100%', 
-    
-    fontWeight:"bold",
-    fontSize:"larger",
-    color:"#FFFF"
-  };
+
+  document.body.style.backgroundImage = 'url(/background.png)';
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundPosition = 'top';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.height ='30em';
 
   return (
     <Container>
-      <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 50 }}>
-        Saber y Ganar
+        <CssBaseline />
+      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 20 }}>
+        Saber y Ganar: El Juego
       </Typography>
     </Container>
     );
