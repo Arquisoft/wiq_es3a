d16f75a8e7a5d61df75a798efab5a27abe8576ec
marcosMachadoMenendez <machado14.mm@gmail.com>
2024-03-03 12:37:33 +0100

    Estilo aplicado a barra de navegación

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Login';
-import Navbar from "./Navbar";
 
 function Home() {
 
--- webapp/src/components/Navbar.js
+++ webapp/src/components/Navbar.js
@@ -1,40 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { IoClose, IoMenu } from "react-icons/io5";
+import "./Navbar.css";
 
 const Navbar = () => {
  return (
    <header className="header">
      <nav className="nav container">
-       <NavLink to="/" className="nav__logo">
-         Navigation Bar
-       </NavLink>
 
        <div
-         className={"nav__menu"}
+         className={"nav-menu"}
          id="nav-menu"
        >
-         <ul className="nav__list">
-           <li className="nav__item">
-             <NavLink to="/" className="nav__link">
-               Login
+         <ul className="nav-list">
+            <li className="nav-item">
+             <NavLink to="/user" className="nav__link">
+               Usuario
              </NavLink>
-           </li>
-           <li className="nav__item">
+            </li>
+
+           <li className="nav-item">
              <NavLink to="/home" className="nav__link">
                Home
              </NavLink>
            </li>
+           
+           <li className="nav-item">
+             <NavLink to="/" className="nav__link">
+               Login
+             </NavLink>
+           </li>
           
          </ul>
-         <div className="nav__close" id="nav-close">
-           <IoClose />
-         </div>
+        
        </div>
 
-       <div className="nav__toggle" id="nav-toggle">
-         <IoMenu />
-       </div>
      </nav>
    </header>
  );
