854763343c96918243d86e77c57bf60a8a93b142
UO289659 <uo289659@uniovi.es>
2024-03-06 20:45:25 +0100

    Quitado un cambio para poder hacer merge de la documentación sin incluir ese cambio.

--- users/userservice/user-model.js
+++ users/userservice/user-model.js
@@ -13,16 +13,6 @@ const userSchema = new mongoose.Schema({
       type: Date,
       default: Date.now, 
     },
-    gamesPlayed:{ //Añadido para guardar las partidas en la bbdd
-      type: Number,
-    },
-    rigthAnswers:{
-      type:Number,
-    },
-    wrongAnswers:{
-      type:Number,
-    },
-    
 });
 
 const User = mongoose.model('User', userSchema);
