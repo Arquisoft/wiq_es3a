c2dcca2120d0bb51bd3c7a59e449c0228936418b
UO277382 <UO277382@uniovi.es>
2024-03-08 22:45:59 +0100

    Arreglo Test Primera

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders primera', () => {
   render(<App />);
   const linkElement = screen.getByText(/Saber y Ganar: El Juego/i);
   expect(linkElement).toBeInTheDocument();
