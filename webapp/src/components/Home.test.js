c66f7eb48be8cb649834a15c890e285f7bb51cce
marcosMachadoMenendez <machado14.mm@gmail.com>
2024-03-10 16:20:12 +0100

    Cambios para tests

--- webapp/src/components/Home.test.js
+++ webapp/src/components/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Home from './Home';
 import userEvent from '@testing-library/user-event';
 import {BrowserRouter} from 'react-router-dom';
