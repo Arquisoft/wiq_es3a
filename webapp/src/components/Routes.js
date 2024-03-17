9355b3f3696104e901e573bf3a6c36c0c55a92fe
marcosMachadoMenendez <machado14.mm@gmail.com>
2024-03-17 15:56:17 +0100

    Token de login implementado

---
+++ webapp/src/components/Routes.js
@@ -0,0 +1,64 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useAuth } from "./login/AuthProvider";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Home from "./Home";
+import Inicio from './Inicio';
+import Primera from './Primera';
+import Game from './QuizGame';
+import Navbar from "./Navbar";
+
+const Routes = () => {
+  const { token } = useAuth();
+
+  // Define public routes accessible to all users
+  const routesForPublic = [
+    {
+      path: "/",
+      element: <Primera />,
+    },
+    {
+      path: "/login",
+      element: <Inicio />,
+    },
+  ];
+
+  // Define routes accessible only to authenticated users
+  const routesForAuthenticatedOnly = [
+    {
+      path: "/",
+      element: <ProtectedRoute />, // Wrap the component in ProtectedRoute
+      children: [
+        {
+          path: "/",
+          element: <Primera />,
+        },
+        {
+          path: "/game",
+          element: <Game />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
+  ];
+
+  // Define routes accessible only to non-authenticated users
+  const routesForNotAuthenticatedOnly = [
+    
+  ];
+
+  // Combine and conditionally include routes based on authentication status
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...(!token ? routesForNotAuthenticatedOnly : []),
+    ...routesForAuthenticatedOnly,
+  ]);
+
+  // Provide the router configuration using RouterProvider
+  return <RouterProvider router={router}> <Navbar /> </RouterProvider>
+  ;
+};
+
+export default Routes;
