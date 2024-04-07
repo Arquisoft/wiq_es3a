82d51a904b9d53901809ca221f6fc7f1d6d991fa
Felipe Garcia <uo285267@uniovi.es>
2024-04-07 13:56:04 +0200

    Hotfix Update gateway endpoint in Statistics.js

--- webapp/src/components/Statistics.js
+++ webapp/src/components/Statistics.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Statistics.css';
 
 const Statistics= () => {
-  const gatewayEndpoint = process.env.GATEWAY_SERVICE_URL || 'http://localhost:8000';
+  const gatewayEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null); 
 
