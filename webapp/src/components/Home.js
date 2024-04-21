2e9057c01d112c45c2751470516279ff8a72340c
UO277382 <UO277382@uniovi.es>
2024-04-21 18:10:58 +0200

    Cambio parametros funcional. Faltan test

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -3,29 +3,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import {Radio, RadioGroup, FormControl, FormControlLabel, FormLabel } from '@mui/material';
-import {useAuth} from "./login/AuthProvider";
 import "./Home.css";
+import Parameters from './Parameters';
 
 function Home() {
 
-  const {numeroPreguntas, setNumPreguntas, timepoJuego , setTiempoJuego} = useAuth();
-
-  const iniciarPartida = () =>
-  {
-    window.location.href = '/game';
-  };
-
-  const cambioPara = (event) =>
-  {
-    let v = event.target.value.split("|");
-    setNumPreguntas(v[0]);
-    setTiempoJuego(v[1]);
-  };
 
   return (
     <div className="home">
-
       <img className="imagenHome" src="/saber-ganar-logo.png" alt="Logo" />
 
       <Container className="boxHome" maxWidth="xs">
@@ -38,27 +23,11 @@ function Home() {
         <Typography className="tituloHome" component="h2" variant="h5" align="center" sx={{ marginTop: 2 }}>
         PULSA EL BOTÓN PARA JUGAR
         </Typography>
-        <Button className="botonHome" variant="contained" color="primary" fullWidth onClick={iniciarPartida}>
+        <Button className="botonHome" variant="contained" color="primary" fullWidth onClick={window.location.href = '/game'}>
           JUGAR
         </Button>
-      </Container>      
-      <Container className='boxHome' maxWidth="xs">
-        <FormControl>
-          <FormLabel>PARÁMETROS DE LA PARTIDA:</FormLabel>
-          <RadioGroup
-          aria-labelledby="demo-controlled-radio-buttons-group"
-          name="controlled-radio-buttons-group"
-          defaultValue={"9|150"}
-          value={''}
-          onChange={cambioPara}>
-            <FormControlLabel value="9|150" control={<Radio />} label="Predeterminado"/>              
-            <FormControlLabel value="5|60" control={<Radio />} label="Partida Corta"/>                           
-            <FormControlLabel value="10|90" control={<Radio />} label="Partida Media"/>                          
-            <FormControlLabel value="20|210" control={<Radio />} label="Partida Larga"/>               
-          </RadioGroup>
-        </FormControl>
       </Container>
-      
+      <Parameters />            
     </div>
   );
 }
---
+++ webapp/src/components/Parameters.js
@@ -0,0 +1,45 @@
+
+import React, { useEffect } from 'react';
+import Container from '@mui/material/Container';
+import {Radio, RadioGroup, FormControl, FormControlLabel, FormLabel } from '@mui/material';
+import {useAuth} from "./login/AuthProvider";
+function Parameters(){
+    
+
+  const {setNumPreguntas, setTiempoJuego} = useAuth();
+
+    const cambioPara = (event) =>
+    {
+      let v = event.target.value.split("|");
+      setNumPreguntas(v[0]);
+      setTiempoJuego(v[1]);
+    };
+
+    useEffect(()=>
+    {
+      setNumPreguntas("9");
+      setTiempoJuego("150");
+    });
+
+    return (
+
+    <Container className='boxHome' maxWidth="xs">
+        <FormControl>
+        <FormLabel>PARÁMETROS DE LA PARTIDA:</FormLabel>
+        <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        defaultValue={"9|150"}
+        value={''}
+        onChange={cambioPara}>
+            <FormControlLabel value="9|150" control={<Radio />} label="Predeterminado" id="predeterminado"/>              
+            <FormControlLabel value="5|60" control={<Radio />} label="Partida Corta" id="corta"/>                           
+            <FormControlLabel value="10|90" control={<Radio />} label="Partida Media" id="media"/>                          
+            <FormControlLabel value="20|210" control={<Radio />} label="Partida Larga" id="larga"/>               
+        </RadioGroup>
+        </FormControl>
+    </Container>
+    );
+}
+
+export default Parameters;
